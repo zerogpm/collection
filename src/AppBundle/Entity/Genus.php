7227052b76c7747b10f6eead41593553fd2ddff3
zerogpm <zerogpm@gmail.com>
2017-05-06 11:49:21 -0700

    fetching items from a many to many collection

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -185,4 +185,14 @@ class Genus
         }
         $this->genusScientists[] = $user;
     }
+
+    /**
+     * @return ArrayCollection|User[]
+     */
+    public function getGenusScientists()
+    {
+        return $this->genusScientists;
+    }
+
+
 }
